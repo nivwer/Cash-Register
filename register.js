e654bd12afabdee310195922411acc4c80ba6af4
nivwer <nivwerr@gmail.com>
2023-06-26 17:16:14 -0300

    update register.js

--- register.js
+++ register.js
@@ -81,15 +81,15 @@ function checkCashRegister(price, cash, cid) {
   document.getElementById("cash-register-form").addEventListener("submit", handleFormSubmit);
   
   const defaultCID = [
-    ["PENNY", 0],
-    ["NICKEL", 0],
-    ["DIME", 0],
-    ["QUARTER", 0],
-    ["ONE", 0],
-    ["FIVE", 0],
-    ["TEN", 0],
-    ["TWENTY", 0],
-    ["ONE HUNDRED", 0]
+    ["PENNY", 1.01],
+    ["NICKEL", 2.05],
+    ["DIME", 3.1],
+    ["QUARTER", 4.25],
+    ["ONE", 90],
+    ["FIVE", 55],
+    ["TEN", 20],
+    ["TWENTY", 60],
+    ["ONE HUNDRED", 100]
   ];
   
   function createCIDField(currency, amount) {
